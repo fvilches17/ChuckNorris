fedefbf9c1330316f4f06d062b5e794866e1a84c
Francisco Vilches <f_vilches17@outlook.com>
2018-05-31 02:52:31 +1200

    added front end UI notification logic

--- ChuckNorris.Web/wwwroot/js/app.js
+++ ChuckNorris.Web/wwwroot/js/app.js
@@ -15,4 +15,23 @@
 
 $(document).ready(function () {
     highlightPageIcon();
+
+    if (Notification.permission === "granted") {
+        const notificationIcon = $("#notification-icon");
+        notificationIcon.css({ '-webkit-filter': 'grayscale(0)', 'filter': 'grayscale(0)' });
+    }
+
+    $("#notification-icon").on("click", function () {
+        const clickedElement = $(this);
+
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission().then(permission => {
+
+                Notification.permission === "granted"
+                    ? clickedElement.css({ '-webkit-filter': 'grayscale(0)', 'filter': 'grayscale(0)' })
+                    : clickedElement.css({ '-webkit-filter': 'grayscale(100%)', 'filter': 'grayscale(100%)' });
+
+            });
+        }
+    });
 });
