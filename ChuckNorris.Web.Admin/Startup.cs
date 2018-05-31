9b0cdd420181d3f2186d03adb8d6ccfe2c2997a3
Francisco Vilches <f_vilches17@outlook.com>
2018-05-31 23:05:15 +1200

    dynamically adding api connection string in admin portal  based on environment

--- ChuckNorris.Web.Admin/Startup.cs
+++ ChuckNorris.Web.Admin/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,17 +7,20 @@ namespace ChuckNorris.Web.Admin
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddScoped<IFactsClient, FactsClient>();
+            services.AddScoped<IFactsClient, FactsClient>((foo) =>
+            {
+                return new FactsClient { BaseUrl = Configuration["apiBaseUrl"] };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
