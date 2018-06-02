e88280bb8b0a2af99f4f346269b62c3b81da00d2
Francisco Vilches <f_vilches17@outlook.com>
2018-06-02 19:18:51 +1200

    added more default cuck facts

--- ChuckNorris.Api/AppContext.cs
+++ ChuckNorris.Api/AppContext.cs
@@ -20,8 +20,12 @@ namespace ChuckNorris.Api
             var facts = new List<Fact>
             {
                 new Fact{Description = "Chuck Norris mined all bitcoins...twice"},
+                new Fact{Description = "The Great Wall of China was originally created to keep Chuck Norris out. It failed miserably."},
                 new Fact{Description = "Chuck Norris beat the sun in a staring contest"},
-                new Fact{Description = "Chuck Norris narrates Morgan Freeman's life"}
+                new Fact{Description = "Chuck Norris narrates Morgan Freeman's life"},
+                new Fact{Description = "When chuck Norris does division, there are no remainders"},
+                new Fact{Description = "There are two types of people in the world... people that suck, and Chuck Norris"},
+                new Fact{Description = "If at first you don't succeed, you're not Chuck Norris."}
             };
 
             Facts.AddRange(facts);
